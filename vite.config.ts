f00a3cd3d61984e8265ed463229abe60637ef830
ankxd <reynal@mail.ru>
2024-10-17 21:46:08 +0300

    gh pages init

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/testtask/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
 })
